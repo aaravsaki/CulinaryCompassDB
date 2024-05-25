867ae020b237074a7b308f0e101b0ff9318f787f
Brandon Khoi Hoang <brandokh@uci.edu>
2024-05-24 23:03:12 -0700

    ID Verification Fixed

--- queries2.py
+++ queries2.py
@@ -4,6 +4,4 @@ supabase = init.get_client()
 
 def verify_id(name: str) -> bool:
     response = supabase.table("person").select("user_id").eq("username", name).execute()
-    return len(response.data)
-
-print(verify_id("test"))
+    return len(response.data) != 0
