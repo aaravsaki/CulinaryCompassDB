133b13f0c64d81643f23a6ffe2e8afbfb7d25cfc
Brandon Khoi Hoang <brandokh@uci.edu>
2024-05-25 02:44:24 -0700

    Generalized get statement

--- queries.py
+++ queries.py
@@ -12,17 +12,7 @@ def verify_id(name: str) -> bool:
     response = supabase.table("person").select("user_id").eq("username", name).execute()
     return len(response.data) != 0
 
-#gets the rows pertaining to meals
-def get_meal(mid: int):
-    response = supabase.table("meal").select("*").eq("meal_id", mid).execute()
-    return response.data
-
-#gets the rows pertaining to the fooditem
-def get_fooditem(fid: int):
-    response = supabase.table("fooditem").select("*").eq("item_id", fid).execute()
-    return response.data
-
-#gets rows pertaining to meal_has
-def get_foods(mid: int):
-    response = supabase.table("meal_has").select("*").eq("meal_id", mid).execute()
+#generic get statement execution
+def execute_get(tablename: str, attr_name: str, pid: int):
+    response = supabase.table(tablename).select("*").eq(attr_name, pid).execute()
     return response.data
