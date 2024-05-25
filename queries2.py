dd4084216d2628aca8a3a68dfd97c3f8aeed1d96
Brandon Khoi Hoang <brandokh@uci.edu>
2024-05-24 22:40:43 -0700

    ID Verification

---
+++ queries2.py
@@ -0,0 +1,7 @@
+import init
+
+supabase = init.get_client()
+
+def verify_id(name: str) -> bool:
+    response = supabase.table("person").select("user_id").eq("username", name).execute()
+    return len(response.data)
