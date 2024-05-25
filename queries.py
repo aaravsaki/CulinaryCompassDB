ad3666a83d74bf3d5aa1bea0660269a38deb09dc
Aarav Saki <asaki@uci.edu>
2024-05-24 23:02:18 -0700

    sql insert function completed

--- queries.py
+++ queries.py
@@ -2,4 +2,8 @@ import init
 
 supabase = init.get_client()
 
+def execute_insert_statement(tablename: str, columns: list[str], values: list[str]):
+    data = {col: val for col, val in zip(columns, values)}
+    supabase.table(tablename).upsert(data).execute()
+
 
