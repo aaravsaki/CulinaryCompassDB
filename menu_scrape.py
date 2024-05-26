9ea658d64d4ef9169dbd799601614766e08b2e34
Brandon Khoi Hoang <brandokh@uci.edu>
2024-05-25 19:01:22 -0700

    menu_scrape now accomodates anteatery

--- menu_scrape.py
+++ menu_scrape.py
@@ -2,23 +2,42 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
 brandywine = "https://uci.campusdish.com/en/locationsandmenus/brandywine/"
+anteatery = "https://uci.campusdish.com/en/locationsandmenus/theanteatery/"
 path = 'C:/Users/brand/Downloads/chromedriver/chromedriver-win64/chromedriver.exe'
 service = Service(executable_path=path)
 driver = webdriver.Chrome(service=service)
 
-driver.get(brandywine)
+def get_brandywine_menu():
+    driver.get(brandywine)
+    item_xpath = '//h3[contains(@class, "HeaderItemName")]'
 
-#station_xpath = '//h2[contains(@class, "StationHeaderTitle")]'
-#category_xpath = '//h2[contains(@class, "CategoryName")]'
-item_xpath = '//h3[contains(@class, "HeaderItemName")]'
-#calorie_xpath = '//span[contains(@class, "ItemCalories")]'
+    file = create_file("BrandywineMenu.txt", "Brandywine's Food Options!")
+
+    items = driver.find_elements(by='xpath', value = item_xpath)
+    fill_out_menu(items, file)
+    file.close()
+
+def get_anteatery_menu():
+    driver.get(anteatery)
+    item_xpath = '//span[contains(@class, "HeaderItemNameLink")]'
+
+    file = create_file("AnteateryMenu.txt", "Anteatery's Food Options!")
+    items = driver.find_elements(by='xpath', value = item_xpath)
+    fill_out_menu(items, file)
+    file.close()
 
 
-items = driver.find_elements(by='xpath', value = item_xpath)
-#calories = driver.find_elements(by='xpath', value = calorie_xpath)
+def create_file(name: str, title: str):
+    file = open(name, "w")
+    file.write(f"{title}\n")
+    file.write("-----------------------------\n")
+    return file
 
-file = open("fooditems.txt", "w")
+def fill_out_menu(info: list[str], f: str):
+    for item in info:
+        f.write(f"{item.text}\n")
 
-for item in items:
-    file.write(f"{item.text}\n")
+def main():
+    get_anteatery_menu()
+    get_brandywine_menu()
 
