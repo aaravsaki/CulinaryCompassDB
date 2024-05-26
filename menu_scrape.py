8d5d4d41276da6810e2e90367544f8442915c5b7
Brandon Khoi Hoang <brandokh@uci.edu>
2024-05-25 18:22:57 -0700

    writes to file the breakfast options for the day

--- menu_scrape.py
+++ menu_scrape.py
@@ -1,3 +1,24 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 
+brandywine = "https://uci.campusdish.com/en/locationsandmenus/brandywine/"
+path = 'C:/Users/brand/Downloads/chromedriver/chromedriver-win64/chromedriver.exe'
+service = Service(executable_path=path)
+driver = webdriver.Chrome(service=service)
+
+driver.get(brandywine)
+
+#station_xpath = '//h2[contains(@class, "StationHeaderTitle")]'
+#category_xpath = '//h2[contains(@class, "CategoryName")]'
+item_xpath = '//h3[contains(@class, "HeaderItemName")]'
+#calorie_xpath = '//span[contains(@class, "ItemCalories")]'
+
+
+items = driver.find_elements(by='xpath', value = item_xpath)
+#calories = driver.find_elements(by='xpath', value = calorie_xpath)
+
+file = open("fooditems.txt", "w")
+
+for item in items:
+    file.write(f"{item.text}\n")
 
